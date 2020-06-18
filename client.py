a1ba57a0dcae5d66c8cfd08df3b636a78a17b525
unknown <elisheva054@gmail.com>
2020-06-18 11:20:04 +0300

    add server and client for checking

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket()
+s.connect(('127.0.0.1',65531))
+while True:
+    str = input("\nMessage: ")
+    s.send(str.encode());
+    #if(str == "Bye" or str == "bye"):
+        #break
+s.close()
